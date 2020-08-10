563d1765346c3924b1289c735bc13dcc406a809d
PeperoJo <hanjoseph11@gmail.com>
2020-08-10 16:21:41 -0400

    article

--- js/misc.js
+++ js/misc.js
@@ -1,12 +1,5 @@
 /* Open when someone clicks on the span element */
-function openNav() {
-  document.getElementById("myNav").style.width = "300px";
-}
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
 
 document.documentElement.style.overflow = 'hidden';
 document.body.scroll = "no";
